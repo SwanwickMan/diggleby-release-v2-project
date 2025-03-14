127c94668c769c113d23713599208a082998f6ab
Andrew Laing <andrewlaing76@gmail.com>
2025-03-14 00:17:56 +0000

    argh ive complete the assignment spongebob

--- src/Interp.java
+++ src/Interp.java
@@ -1,8 +1,10 @@
 import ast.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Interp {
     private Scanner reader = new Scanner(System.in);
@@ -38,13 +40,116 @@ public class Interp {
     // Given a value and a type, performs a cast (according to the rules in the spec) if possible,
     // raising a CastException otherwise.
     private Value cast(Value vFrom, Type ty) throws CastException {
-        throw new RuntimeException("Fill in 'cast' in Interp.java");
+        return switch (ty) {
+            case TyString ignored -> switch (vFrom) {
+                case VInt vi -> new VString(Integer.toString(vi.getValue()));
+                case VBool vb -> new VString(vb.getValue() ? "true" : "false");
+                case VString vs -> vs;
+                case VUnit ignoredToo -> new VString("()");
+                default -> throw new CastException("Bad cast: " + vFrom + " to String");
+            };
+
+            case TyInt ignored -> switch (vFrom) {
+                case VString vs when vs.getValue().matches("-?\\d+") -> new VInt(Integer.parseInt(vs.getValue()));
+                case VBool vb -> new VInt(vb.getValue() ? 1 : 0);
+                case VInt vi -> vi;
+                default -> throw new CastException("Bad cast: " + vFrom + " to Int");
+            };
+
+            case TyUnit ignored -> switch (vFrom) {
+                case VUnit vu -> vu;
+                default -> throw new CastException("Bad cast: " + vFrom + " to Unit");
+            };
+
+            case TyBool ignored -> switch (vFrom) {
+                case VBool vb -> vb;
+                case VString vs when vs.getValue().equals("true") -> new VBool(true);
+                case VString vs when vs.getValue().equals("false") -> new VBool(false);
+                default -> throw new CastException("Invalid cast from " + vFrom + " to Bool");
+            };
+
+            default -> throw new CastException("Bad cast: " + vFrom + " to " + ty);
+        };
     }
 
     // Given a map from function names to function definitions, and a value environment,
     // return the value resulting from evaluating expression e
     public Value interpExpr(Map<String, FnDef> fnDefs, Environment<Value> valEnv, Expr e) {
-        throw new RuntimeException("Fill in 'interpExpr' in Interp.java");
+        return switch (e) {
+            case EInt ei -> new VInt(ei.getValue());
+            case EBool eb -> new VBool(eb.getValue());
+            case EString es -> new VString(es.getValue());
+            case EUnit ignored -> new VUnit();
+            case EVar ev -> valEnv.lookup(ev.getVar());
+
+            case EBinOp binOp -> evalBinOp(binOp.getOp(),
+                    interpExpr(fnDefs, valEnv, binOp.getE1()),
+                    interpExpr(fnDefs, valEnv, binOp.getE2()));
+
+            case ENot en -> new VBool(!unwrapBool(interpExpr(fnDefs, valEnv, en.getExpr())));
+            case ELet el -> {
+                Value boundVal = interpExpr(fnDefs, valEnv, el.getSubject());
+                yield interpExpr(fnDefs, valEnv.extend(el.getBinder(), boundVal), el.getContinuation());
+            }
+            case ECond ec -> unwrapBool(interpExpr(fnDefs, valEnv, ec.getTest()))
+                    ? interpExpr(fnDefs, valEnv, ec.getThenBranch())
+                    : interpExpr(fnDefs, valEnv, ec.getElseBranch());
+            case EFnCall efc -> {
+                FnDef fn = fnDefs.get(efc.getFnName());
+                if (fn == null) throw new RuntimeException("Function not defined: " + efc.getFnName());
+                List<Value> argValues = efc.getArgs().stream()
+                        .map(arg -> interpExpr(fnDefs, valEnv, arg))
+                        .toList();
+                Environment<Value> newEnv = valEnv;
+                for (int i = 0; i < fn.getParams().size(); i++) {
+                    newEnv = newEnv.extend(fn.getParams().get(i).param, argValues.get(i));
+                }
+                yield interpExpr(fnDefs, newEnv, fn.getBody());
+            }
+            case EConcat ec -> {
+                String result = ec.getExprs().stream()
+                        .map(arg -> unwrapString(interpExpr(fnDefs, valEnv, arg)))
+                        .collect(Collectors.joining());
+                yield new VString(result);
+            }
+            case EPrint ep -> {
+                System.out.println(unwrapString(interpExpr(fnDefs, valEnv, ep.getExpr())));
+                yield new VUnit();
+            }
+            case EGetInput ignored -> new VString(reader.nextLine());
+            case ECast ec -> {
+                Value val = interpExpr(fnDefs, valEnv, ec.getExpr());
+                try {
+                    yield cast(val, ec.getType());
+                } catch (CastException ex) {
+                    throw new RuntimeException("Invalid cast: " + ex.getMessage());
+                }
+            }
+            default -> throw new RuntimeException("Unrecognized expression type: " + e);
+        };
+    }
+
+    private Value evalBinOp(EBinOp.Op op, Value left, Value right) {
+        return switch (op) {
+            case ADD -> new VInt(unwrapInt(left) + unwrapInt(right));
+            case SUB -> new VInt(unwrapInt(left) - unwrapInt(right));
+            case MUL -> new VInt(unwrapInt(left) * unwrapInt(right));
+            case DIV -> {
+                int r = unwrapInt(right);
+                if (r == 0) throw new RuntimeException("Division by zero");
+                yield new VInt(unwrapInt(left) / r);
+            }
+            case MOD -> {
+                int r = unwrapInt(right);
+                if (r == 0) throw new RuntimeException("Modulo by zero");
+                yield new VInt(unwrapInt(left) % r);
+            }
+            case GT  -> new VBool(unwrapInt(left) > unwrapInt(right));
+            case LT  -> new VBool(unwrapInt(left) < unwrapInt(right));
+            case EQ  -> new VBool(left.equals(right)); // Works for all types
+            case AND -> new VBool(unwrapBool(left) && unwrapBool(right));
+            case OR  -> new VBool(unwrapBool(left) || unwrapBool(right));
+        };
     }
 
     // Runs a program by constructing a map from function names to function definitions,
--- src/Typecheck.java
+++ src/Typecheck.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Map;
 
 import ast.*;
@@ -10,16 +11,110 @@ public class Typecheck {
         }
     }
 
-    public Type typecheckExpr(Map<String, FnDef> fnDefs, Environment<Type> tyEnv, Expr e) {
-        throw new RuntimeException("Fill in 'typecheckExpr' in Typecheck.java");
-    }
-
     public void typecheckDef(Map<String, FnDef> defs, FnDef def) {
-        throw new RuntimeException("Fill in 'typecheckDef' in Typecheck.java");
+        Environment<Type> paramEnv = new Environment<>();
+        for (AnnotatedParam param : def.getParams()) {
+            paramEnv = paramEnv.extend(param.param, param.type);
+        }
+        Type bodyType = typecheckExpr(defs, paramEnv, def.getBody());
+        checkType(def.getReturnType(), bodyType);
     }
 
     public Type typecheckProgram(Program p) {
-        throw new RuntimeException("Fill in 'typecheckProgram' in Typecheck.java");
+        Map<String, FnDef> functionMap = new HashMap<>();
+        for (FnDef fn : p.getFunctionDefs()) {
+            functionMap.put(fn.getFnName(), fn);
+        }
+        for (FnDef fn : p.getFunctionDefs()) {
+            typecheckDef(functionMap, fn);
+        }
+        return typecheckExpr(functionMap, new Environment<>(), p.getMainExpr());
     }
 
+
+
+    public Type typecheckExpr(Map<String, FnDef> fnDefs, Environment<Type> tyEnv, Expr e) {
+        return switch (e) {
+            case EInt ignored -> TyInt.type();
+            case EBool ignored -> TyBool.type();
+            case EString ignored -> TyString.type();
+            case EUnit ignored -> TyUnit.type();
+            case EVar ev -> tyEnv.lookup(ev.getVar());
+            case EBinOp binOp -> {
+                Type leftType = typecheckExpr(fnDefs, tyEnv, binOp.getE1());
+                Type rightType = typecheckExpr(fnDefs, tyEnv, binOp.getE2());
+                EBinOp.Op op = binOp.getOp();
+                yield switch (op) {
+                    case ADD, SUB, MUL, DIV, MOD -> {
+                        checkType(TyInt.type(), leftType);
+                        checkType(TyInt.type(), rightType);
+                        yield TyInt.type();
+                    }
+                    case GT, LT -> {
+                        checkType(TyInt.type(), leftType);
+                        checkType(TyInt.type(), rightType);
+                        yield TyBool.type();
+                    }
+                    case EQ -> {
+                        checkType(leftType, rightType);
+                        yield TyBool.type();
+                    }
+                    case AND, OR -> {
+                        checkType(TyBool.type(), leftType);
+                        checkType(TyBool.type(), rightType);
+                        yield TyBool.type();
+                    }
+                };
+            }
+            case ENot en -> {
+                Type exprType = typecheckExpr(fnDefs, tyEnv, en.getExpr());
+                checkType(TyBool.type(), exprType);
+                yield TyBool.type();
+            }
+            case ELet el -> {
+                Type boundType = typecheckExpr(fnDefs, tyEnv, el.getSubject());
+                Environment<Type> newEnv = tyEnv.extend(el.getBinder(), boundType);
+                yield typecheckExpr(fnDefs, newEnv, el.getContinuation());
+            }
+            case ECond ec -> {
+                Type condType = typecheckExpr(fnDefs, tyEnv, ec.getTest());
+                checkType(TyBool.type(), condType);
+                Type thenType = typecheckExpr(fnDefs, tyEnv, ec.getThenBranch());
+                Type elseType = typecheckExpr(fnDefs, tyEnv, ec.getElseBranch());
+                checkType(thenType, elseType);
+                yield thenType;
+            }
+            case EFnCall efc -> {
+                FnDef fn = fnDefs.get(efc.getFnName());
+                if (fn == null) {
+                    throw new TypeErrorException("Undefined function: " + efc.getFnName());
+                }
+                if (fn.getParams().size() != efc.getArgs().size()) {
+                    throw new TypeErrorException("Incorrect number of arguments for function: " + efc.getFnName());
+                }
+                for (int i = 0; i < fn.getParams().size(); i++) {
+                    Type expected = fn.getParams().get(i).type;
+                    Type actual = typecheckExpr(fnDefs, tyEnv, efc.getArgs().get(i));
+                    checkType(expected, actual);
+                }
+                yield fn.getReturnType();
+            }
+            case EConcat ec -> {
+                for (Expr expr : ec.getExprs()) {
+                    checkType(TyString.type(), typecheckExpr(fnDefs, tyEnv, expr));
+                }
+                yield TyString.type();
+            }
+            case EPrint ep -> {
+                checkType(TyString.type(), typecheckExpr(fnDefs, tyEnv, ep.getExpr()));
+                yield TyUnit.type();
+            }
+            case EGetInput ignored -> TyString.type();
+            case ECast ec -> {
+                typecheckExpr(fnDefs, tyEnv, ec.getExpr());
+                yield ec.getType();
+            }
+            default -> throw new TypeErrorException("Bad type not recognised: " + e);
+        };
+    }
 }
